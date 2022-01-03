b6801893c2e18e4dbe14df7d48a6d039aa3b6c2d
claudiocassiano <claudiopecassiano@gmail.com>
2022-01-03 18:57:23 -0300

    Atualizando o testeGit
    Atualização

--- testeGit.py
+++ testeGit.py
@@ -1,9 +1,10 @@
 
 print('Testando o git!')
 
-nota=int(input('Digite um valor: '))
+nota=int(input('Digite uma nota entre 0 e 10: '))
 if nota > 10:
-    print('Nota maior que 10.') 
-
+    print(f'Sua nota foi {nota}. A nota foi maior que 10.') 
+elif nota < 10:
+    print(f'Sua nota foi {nota}. A nota foi menor que 10.')
 
 
